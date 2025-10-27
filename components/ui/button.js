d323b39cfdb94cd677a7ac7d2ff408355b2857e8
khedmeapp-Admin <khedme.app@gmail.com>
2025-10-27 22:47:14 +0200

    Add missing Button component for admin approve page

---
+++ components/ui/button.js
@@ -0,0 +1,13 @@
+"use client";
+import React from "react";
+
+export function Button({ children, className = "", ...props }) {
+  return (
+    <button
+      {...props}
+      className={`px-4 py-2 rounded-lg font-medium transition ${className}`}
+    >
+      {children}
+    </button>
+  );
+}
